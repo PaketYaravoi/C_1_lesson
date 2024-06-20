ce54abb7d530abec88657c59eaeda8e1463dcb69
GroshkovR <rik1810huragan@gmail.com>
2024-06-20 13:49:44 +1100

    дописал программу на Треугольник Серпинского, параметрами удовлетворяющими запросу - посмотреть в терминале как он выглядит

--- magic/Program.cs
+++ magic/Program.cs
@@ -2,14 +2,14 @@
 Console.SetCursorPosition(10, 4);
 Console.WriteLine("$"); */
 
-int xa = 10;
+int xa = 25;
 int ya = 5;
 
 int xb = 1;
-int yb = 15;
+int yb = 32;
 
-int xc = 20;
-int yc = 15;
+int xc = 50;
+int yc = 32;
 
 /* int xa= 1, ya=1,
        xb= 2, yb=2,
@@ -17,37 +17,41 @@ int yc = 15;
        пример сокращения кода 
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("№");
+Console.WriteLine("*");
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("$");
+Console.WriteLine("*");
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("$"); */
+Console.WriteLine("*"); 
+*/
 
 int x = xa;
 int y = ya;
 
 int count = 0;
 
-while(count < 400)
+while(count < 11000)
 {
     int num = new Random().Next(0, 3); // [0;3) 0 1 2
     if(num == 0 )
     {
         x = ( x + xa) / 2;
         y = ( y + ya) / 2;
+       
     }
     if(num == 1)
     {
         x = ( x + xb) / 2;
         y = ( y + yb) / 2;
+        
     }
     if(num == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
+        
+        
     }
     Console.SetCursorPosition(x , y);
-    Console.WriteLine("*");
+    Console.WriteLine(".");
     count ++;
-
 }
